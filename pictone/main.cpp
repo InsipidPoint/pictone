2a8b841965ba63667e5eadf548b9145954ea3624
Shiwei Song <shiweisong@gmail.com>
2009-11-23 14:20:54 -0600

    opengl integration

--- pictone/main.cpp
+++ pictone/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <GLUT/glut.h>
 #include "Camera.h"
 #include "Detector.h"
 #include "Synth.h"
@@ -10,9 +11,29 @@
 using namespace std;
 using namespace stk;
 
-MultiSynth synth;
-
 const char *WINDOW_NAME = "Pictone";
+
+const IplImage *current_frame;
+IplImage *small_img;
+IplImage *tex_img;
+Camera cam;
+MomentDetector detector;
+MultiSynth synth;
+bool g_detect = false;
+
+//-----------------------------------------------------------------------------
+// function prototypes
+//-----------------------------------------------------------------------------
+void idleFunc( );
+void displayFunc( );
+void reshapeFunc( GLsizei width, GLsizei height );
+void keyboardFunc( unsigned char, int, int );
+void mouseFunc( int button, int state, int x, int y );
+void initialize( );
+
+// width and height of the window
+GLsizei g_width = 640;
+GLsizei g_height = 480;
 
 // This tick() function handles sample computation only.  It will be
 // called automatically when the system needs a new buffer of audio
@@ -20,20 +41,227 @@ const char *WINDOW_NAME = "Pictone";
 int tick( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *dataPointer )
 {
-//    Clarinet *sine = (Clarinet *) dataPointer;
     register StkFloat *samples = (StkFloat *) outputBuffer;
     
     for ( unsigned int i=0; i<nBufferFrames; i++ )
-        *samples++ = synth.tick(); //sine->tick();
+        *samples++ = synth.tick();
     
     return 0;
 }
 
-bool resultPredicate(const pair<CvRect,Detector::Type>& d1, const pair<CvRect,Detector::Type>& d2) {
-    return d1.first.x < d2.first.x;
+//-----------------------------------------------------------------------------
+// Name: initialize( )
+// Desc: sets initial OpenGL states
+//       also initializes any application data
+//-----------------------------------------------------------------------------
+void initialize()
+{
+    // set the GL clear color - use when the color buffer is cleared
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+    // set the shading model to 'smooth'
+    glShadeModel( GL_FLAT );
+    // enable depth
+    glEnable( GL_DEPTH_TEST );
+    // set the front faces of polygons
+    glFrontFace( GL_CCW );
+    // draw in wireframe
+    glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+    
+    // enable lighting for front
+    glLightModeli( GL_FRONT_AND_BACK, GL_TRUE );
+    // material have diffuse and ambient lighting 
+    glColorMaterial( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE );
+    // enable color
+    glEnable( GL_COLOR_MATERIAL );
+}
+
+//-----------------------------------------------------------------------------
+// Name: reshapeFunc( )
+// Desc: called when window size changes
+//-----------------------------------------------------------------------------
+void reshapeFunc( GLsizei w, GLsizei h )
+{
+    // save the new window size
+    g_width = w; g_height = h;
+    // map the view port to the client area
+    glViewport( 0, 0, w, h );
+    // set the matrix mode to project
+    glMatrixMode( GL_PROJECTION );
+    // load the identity matrix
+    glLoadIdentity( );
+    // create the viewing frustum
+    gluPerspective( 90.0, (GLfloat) w / (GLfloat) h, .1, 50.0 );
+    // set the matrix mode to modelview
+    glMatrixMode( GL_MODELVIEW );
+    // load the identity matrix
+    glLoadIdentity( );
+    // position the view point
+    gluLookAt( 0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f );
+}
+
+//-----------------------------------------------------------------------------
+// Name: keyboardFunc( )
+// Desc: key event
+//-----------------------------------------------------------------------------
+void keyboardFunc( unsigned char key, int x, int y )
+{
+    Detector::Result r;
+    switch( key ) {
+        case ' ':
+            if (g_detect) {
+                synth.clear();
+                g_detect = false;
+            } else {
+                r = detector.detect(small_img);
+                synth.init(r);
+                g_detect = true;
+            }   
+            break;
+
+        case 'Q':
+        case 'q':
+            exit(1);
+            break;
+    }
+    
+    glutPostRedisplay( );
+}
+
+//-----------------------------------------------------------------------------
+// Name: mouseFunc( )
+// Desc: handles mouse stuff
+//-----------------------------------------------------------------------------
+void mouseFunc( int button, int state, int x, int y )
+{
+    if( button == GLUT_LEFT_BUTTON )
+    {
+        // when left mouse button is down, move left
+        if( state == GLUT_DOWN )
+        {
+        }
+        else
+        {
+        }
+    }
+    else if ( button == GLUT_RIGHT_BUTTON )
+    {
+        // when right mouse button down, move right
+        if( state == GLUT_DOWN )
+        {
+        }
+        else
+        {
+        }
+    }
+    
+    glutPostRedisplay( );
+}
+
+//-----------------------------------------------------------------------------
+// Name: idleFunc( )
+// Desc: callback from GLUT
+//-----------------------------------------------------------------------------
+void idleFunc( )
+{
+    // render the scene
+    glutPostRedisplay( );
+    
+    // reshape
+    // reshapeFunc( g_width, g_height );
+}
+
+//-----------------------------------------------------------------------------
+// Name: displayFunc( )
+// Desc: callback function invoked to draw the client area
+//-----------------------------------------------------------------------------
+void displayFunc( )
+{
+    // clear the color and depth buffers
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    
+    glPushMatrix();
+
+    if((current_frame = cam.GetFrame()) && !g_detect) {
+        cvResize(current_frame, small_img, CV_INTER_LINEAR);
+        cvRectangle(small_img, cvPoint(80,80), cvPoint(560,400), CV_RGB(0,255,0), 1);
+    }
+    
+//    glEnable(GL_TEXTURE_2D);
+//    glBindTexture (GL_TEXTURE_2D, 13);
+//    glTexImage2D(GL_TEXTURE_2D,        //target
+//                 0,                    //level
+//                 GL_RGB,               //internalFormat
+//                 small_img->width,         //width
+//                 small_img->height,        //height
+//                 0,                    //border
+//                 GL_BGR,               //format
+//                 GL_UNSIGNED_BYTE,     //type
+//                 small_img->imageData);    //pointer to image data
+//    glBegin (GL_QUADS);
+//    glTexCoord2f (0.0, 0.0);
+//    glVertex3f (0.0, 0.0, 0.0);
+//    glTexCoord2f (1.0, 0.0);
+//    glVertex3f (10.0, 0.0, 0.0);
+//    glTexCoord2f (1.0, 1.0);
+//    glVertex3f (10.0, 10.0, 0.0);
+//    glTexCoord2f (0.0, 1.0);
+//    glVertex3f (0.0, 10.0, 0.0);
+//    glEnd ();
+
+//    glMatrixMode(GL_PROJECTION);
+//    glPushMatrix();
+//    glLoadIdentity();
+//    gluOrtho2D(0.0,g_width,0.0,g_height);
+//    glMatrixMode(GL_MODELVIEW);
+//    glPushMatrix();
+//    glLoadIdentity();
+    
+    cvConvertImage(small_img, tex_img, CV_CVTIMG_FLIP);
+    glDrawPixels(tex_img->width,tex_img->height,GL_BGR,GL_UNSIGNED_BYTE,tex_img->imageData);
+    
+//    glMatrixMode(GL_PROJECTION);
+//    glPopMatrix();
+//    glMatrixMode(GL_MODELVIEW);
+//    glPopMatrix();
+    
+//    glEnable(GL_DEPTH_TEST);
+//    glDepthFunc(GL_LEQUAL);
+    
+    glPopMatrix();
+    
+    // flush and swap
+    glFlush( );
+    glutSwapBuffers( );
 }
 
 int main(int argc, char **argv) {
+    
+    // initialize GLUT
+    glutInit( &argc, argv );
+    // double buffer, use rgb color, enable depth buffer
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+    // initialize the window size
+    glutInitWindowSize( g_width, g_height );
+    // set the window postion
+    glutInitWindowPosition( 100, 100 );
+    // create the window
+    glutCreateWindow(WINDOW_NAME);
+    
+    // set the idle function - called when idle
+    glutIdleFunc( idleFunc );
+    // set the display function - called when redrawing
+    glutDisplayFunc( displayFunc );
+    // set the reshape function - called when client area changes
+    glutReshapeFunc( reshapeFunc );
+    // set the keyboard function - called on keyboard events
+    glutKeyboardFunc( keyboardFunc );
+    // set the mouse function - called on mouse stuff
+    glutMouseFunc( mouseFunc );
+    
+    // do our own initialization
+    initialize();
+    
+    //
     
     // Set the global sample rate before creating class instances.
     Stk::setSampleRate( 44100.0 );
@@ -55,8 +283,6 @@ int main(int argc, char **argv) {
         return 0;
     }
     
-//    plucked.noteOn(440, 1);
-    
     try {
         dac.startStream();
     }
@@ -66,30 +292,13 @@ int main(int argc, char **argv) {
     }
     ////////
     
-    cvNamedWindow (WINDOW_NAME, CV_WINDOW_AUTOSIZE);
+    small_img = cvCreateImage(cvSize(640,480), 8, 3);
+    tex_img = cvCreateImage(cvSize(640,480), 8, 3);
     
-    const IplImage *current_frame;
-    IplImage *small_img = cvCreateImage(cvSize(640,480), 8, 3);
-    Camera cam;
-    MomentDetector detector;
+    glutMainLoop();
     
-    while((current_frame = cam.GetFrame())) {
-        cvResize(current_frame, small_img, CV_INTER_LINEAR);
-        cvRectangle(small_img, cvPoint(80,80), cvPoint(560,400), CV_RGB(0,255,0), 1);
-        cvShowImage(WINDOW_NAME, small_img);
-        char key = cvWaitKey(10);
-        if (key == ' ') {   // capture
-            Detector::Result r = detector.detect(small_img);
-            synth.init(r);
-            cvShowImage(WINDOW_NAME, small_img);
-            cvWaitKey(999999);
-            synth.clear();
-        } else if(key == 'q') {
-            break;
-        }
-    }
-    
-    cvReleaseImage( &small_img );
+    cvReleaseImage(&small_img);
+    cvReleaseImage(&tex_img);
     
     ////////////
     // Shut down the output stream.
